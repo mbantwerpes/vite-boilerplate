6a8121c079a7ed10728640f77b4782897d0a45ff
mbantwerpes <marcel.blesing@antwerpes.com>
2021-08-18 14:41:42 +0200

    Implement suggested changes

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite';
 import reactRefresh from '@vitejs/plugin-react-refresh';
 
 export default defineConfig({
-  plugins: [reactRefresh],
+  plugins: [reactRefresh()],
   build: {
     outDir: 'dist/app',
   },
